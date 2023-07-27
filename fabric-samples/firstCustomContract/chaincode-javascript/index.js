0de4b0e16a891c79ed7a32a4c5fa4fec74a6520a
rajesh kumar <rajesh.kumar@antiersolutions.com>
2023-07-27 19:38:06 +0530

    first working hyperledger custom project

---
+++ fabric-samples/firstCustomContract/chaincode-javascript/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+"use strict";
+
+const firstCustomContract = require("./lib/firstCustomContract");
+
+module.exports.FirstCustomContract = firstCustomContract;
+module.exports.contracts = [firstCustomContract];
